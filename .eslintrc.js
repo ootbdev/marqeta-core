65be0c731de455723be33ea37d3a0c32545cb1d5
Alvin Lee <alvin@ootbdev.com>
2020-04-19 07:40:48 -0700

    continue setting up package

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  'env': {
+    'es6': true,
+    'jest': true
+  },
+  'extends': 'standard',
+  'globals': {
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018,
+    'sourceType': 'module'
+  },
+  'rules': {
+  }
+}
