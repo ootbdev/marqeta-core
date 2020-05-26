28c582bca353edbec0adab94278decb92a8e791b
Alvin Lee <alvin@ootbdev.com>
2020-05-26 15:46:49 -0700

    removes API folder in prep for using Resources

--- src/API/index.js
+++
@@ -1,7 +0,0 @@
-import ping from './ping'
-import Users from './Users'
-
-export default {
-  ping,
-  Users
-}
--- src/API/ping.js
+++
@@ -1,8 +0,0 @@
-import { makeRequest } from '../utils'
-
-export default async ({ baseURL }) => {
-  return makeRequest({
-    url: `${baseURL}/ping`,
-    method: 'GET'
-  })
-}
